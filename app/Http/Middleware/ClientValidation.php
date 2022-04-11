dbd3b49d5869fb1f6410392bd56493455eb45f8d
Thomas Chong <thomaschong0929@gmail.com>
2022-04-11 15:56:22 +0800

    updated credential

--- app/Http/Middleware/ClientValidation.php
+++ app/Http/Middleware/ClientValidation.php
@@ -14,7 +14,7 @@ class ClientValidation
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    private $credential = 'fuckKonami';
+    private $credential = '0daeed08bfd9c77436a5f94eaac641935f24a17345ce35941aca68e8644ffef59586701130db8a10a4cb408be9b929ed6f7021052186d0adc0ad90ba0401a442';
     
     public function handle(Request $request, Closure $next)
     {
